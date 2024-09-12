06ae42c2f6c307fcbfe07f1ba062f2b5270dcc2a
Developer Ritesh <por.ritesh.org@gmail.com>
2024-09-12 21:00:21 +0530

    Editor V1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_quill_editor
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_quill_editor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_quill_editor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
